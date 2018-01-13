be0ea892c505400fbdd6b056cecf7df9fd49526d
andy.chen <as79726@gmail.com>
2018-01-13 17:04:49 +0800

    commit

---
+++ src/pages/camera/camera.ts
@@ -0,0 +1,62 @@
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  AlertController
+} from "ionic-angular";
+import { Camera, CameraOptions } from "@ionic-native/camera";
+/**
+ * Generated class for the CameraPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: "page-camera",
+  templateUrl: "camera.html"
+})
+export class CameraPage {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private camera: Camera,
+    public alertCtrl: AlertController
+  ) {}
+
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad CameraPage");
+  }
+
+  image: string;
+
+  onTakePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      saveToPhotoAlbum:true
+    };
+
+    this.camera.getPicture(options).then(
+      imageData => {
+        this.image = "data:image/jpeg;base64," + imageData;
+      },
+      err => {
+        this.displayErrorAlert(err);
+      }
+    );
+  }
+
+  displayErrorAlert(err) {
+    let alert = this.alertCtrl.create({
+      title: "Error",
+      subTitle: err,
+      buttons: ["OK"]
+    });
+    alert.present();
+  }
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private alertCtrl: AlertController) {
 
   }
 
-}
+  click(value:string){
+    let alert = this.alertCtrl.create({
+      title: "alertCtrl",
+      subTitle: value,
+      buttons: ["OK"]
+    });
+    alert.present();
+  }
